83d234b287ba61faf71decf2cbe3bf9ececbd2f4
juhwan0702 <ezcredible0702@gmail.com>
2024-11-18 09:46:35 +0900

    fix: :bug: modify image src

--- src/constants/introProducts.ts
+++ src/constants/introProducts.ts
@@ -1,5 +1,5 @@
 export const introProducts = [
   { id: 'intro-product-1', src: '/images/ksnet/KS9000.png', alt: 'KS9000' },
-  { id: 'intro-product-2', src: '/images/ksnet/kS1040S.png', alt: 'KS1040S' },
+  { id: 'intro-product-2', src: '/images/kis/KIS8310.jpg', alt: 'KIS8310' },
   { id: 'intro-product-3', src: '/images/ksnet/CB-TS201.jpg', alt: 'CB-TS201' }
 ];
